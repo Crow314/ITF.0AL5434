f4d7242cf4ae185f490718c68fad8b84d34d39bf
Crow314 <master@crow31415.net>
2023-04-24 04:39:58 +0900

    207: buffer with var list

--- 207/main.go
+++ 207/main.go
@@ -7,43 +7,76 @@ import (
 
 const BUFFER_SIZE int = 8
 
+type buffer struct {
+	buf  chan int
+	rmux sync.Mutex
+	wmux sync.Mutex
+}
+
 func main() {
 	var wg sync.WaitGroup
 
-	b := make(chan int, BUFFER_SIZE)
+	b := buffer{buf: make(chan int, BUFFER_SIZE)}
 
 	wg.Add(1)
-	go threadA(b, &wg)
+	go thread1(&b, &wg)
 
 	wg.Add(1)
-	go threadB(b, &wg)
+	go thread2(&b, &wg)
+
+	wg.Add(1)
+	go thread3(&b, &wg)
 
 	wg.Wait()
 }
 
-func put(b chan<- int, x int) {
-	b <- x
-}
+func put(b *buffer, x []int) {
+	defer b.wmux.Unlock()
+	b.wmux.Lock()
 
-func get(b <-chan int) int {
-	return <-b
-}
-
-func threadA(b chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for i := 0; i < 10; i++ {
-		x := i
-		fmt.Printf("threadA(): put( %d )\n", x)
-		put(b, x)
+	for _, v := range x {
+		b.buf <- v
 	}
 }
 
-func threadB(b <-chan int, wg *sync.WaitGroup) {
+func get(b *buffer, n int) []int {
+	defer b.rmux.Unlock()
+
+	r := make([]int, n)
+
+	b.rmux.Lock()
+
+	for i := 0; i < n; i++ {
+		x := <-b.buf
+		r[i] = x
+	}
+
+	return r
+}
+
+func thread1(b *buffer, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for i := 0; i < 10; i++ {
-		x := get(b)
-		fmt.Printf("threadB(): get() %d\n", x)
+	x := []int{1, 3, 5}
+
+	fmt.Println("Thread 1: put(3,{1,3,5})")
+	put(b, x)
+}
+
+func thread2(b *buffer, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	x := []int{2, 4, 6}
+
+	fmt.Println("Thread 2: put(3,{2,4,6})")
+	put(b, x)
+}
+
+func thread3(b *buffer, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i < 2; i++ {
+		x := get(b, 3)
+		fmt.Printf("thread3(): get() %v\n", x)
 	}
 }

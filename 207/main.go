e9f0e0ec0eaa73242461696956db1e351735bb8e
Crow314 <master@crow31415.net>
2023-04-25 04:23:24 +0900

    207: interleaving

--- 207/main.go
+++ 207/main.go
@@ -7,54 +7,41 @@ import (
 
 const BUFFER_SIZE int = 8
 
-type buffer struct {
-	buf  chan int
-	rmux sync.Mutex
-	wmux sync.Mutex
-}
-
 func main() {
 	var wg sync.WaitGroup
 
-	b := buffer{buf: make(chan int, BUFFER_SIZE)}
+	b := make(chan int, BUFFER_SIZE)
 
 	wg.Add(1)
-	go thread1(&b, &wg)
+	go thread1(b, &wg)
 
 	wg.Add(1)
-	go thread2(&b, &wg)
+	go thread2(b, &wg)
 
 	wg.Add(1)
-	go thread3(&b, &wg)
+	go thread3(b, &wg)
 
 	wg.Wait()
 }
 
-func put(b *buffer, x []int) {
-	defer b.wmux.Unlock()
-	b.wmux.Lock()
-
+func put(b chan<- int, x []int) {
 	for _, v := range x {
-		b.buf <- v
+		b <- v
 	}
 }
 
-func get(b *buffer, n int) []int {
-	defer b.rmux.Unlock()
-
+func get(b <-chan int, n int) []int {
 	r := make([]int, n)
 
-	b.rmux.Lock()
-
 	for i := 0; i < n; i++ {
-		x := <-b.buf
+		x := <-b
 		r[i] = x
 	}
 
 	return r
 }
 
-func thread1(b *buffer, wg *sync.WaitGroup) {
+func thread1(b chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	x := []int{1, 3, 5}
@@ -63,7 +50,7 @@ func thread1(b *buffer, wg *sync.WaitGroup) {
 	put(b, x)
 }
 
-func thread2(b *buffer, wg *sync.WaitGroup) {
+func thread2(b chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	x := []int{2, 4, 6}
@@ -72,7 +59,7 @@ func thread2(b *buffer, wg *sync.WaitGroup) {
 	put(b, x)
 }
 
-func thread3(b *buffer, wg *sync.WaitGroup) {
+func thread3(b <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for i := 0; i < 2; i++ {
